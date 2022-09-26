3a97c92e9ca6c8cbf24ca5058b6c98db7a34fc35
Ozan Akaydin <ozanakaydin@gmail.com>
2022-09-26 18:58:31 +0300

    Connection string configuration was created

--- eCommerceAPI/Presentation/eCommerceAPI.WebApi/Program.cs
+++ eCommerceAPI/Presentation/eCommerceAPI.WebApi/Program.cs
@@ -1,9 +1,12 @@
+using eCommerceAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<eCommerceDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("eCommerceConnectionString")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
